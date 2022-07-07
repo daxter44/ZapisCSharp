5152620a05cf2ae6414efcd537105dd6aec59eeb
Marcin Marcinkowski <marcinek44@windowslive.com>
2022-07-07 21:06:19 +0200

    Initial commit

---
+++ Zapis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace ZapisDoPliku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream file = new FileStream("plik.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            for (int i = 0; i < 101; i++)
+            {
+                file.WriteByte((byte)i);
+            }
+            file.Position = 0;
+            Thread.Sleep(100000);
+            for (int i = 0; i < 101; i++)
+            {
+                Console.WriteLine(file.ReadByte());
+            }
+            file.Close();
+            Console.ReadKey();
+        }
+    }
+}
